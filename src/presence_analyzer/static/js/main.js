7430dc5a38234dfa503aa1a0d87820c013c7d2a0
Daniel Czuba <daniel.czuba@stxnext.pl>
2014-07-07 13:36:17 +0200

    Adding suport for jinja tempaltes for (#3079)

---
+++ src/presence_analyzer/static/js/main.js
@@ -0,0 +1,21 @@
+google.load("visualization", "1", {packages:["corechart", 'timeline'], 'language': 'pl'});
+
+function parseInterval(value) {
+    var result = new Date(1,1,1);
+    result.setMilliseconds(value*1000);
+    return result;
+}
+
+(function($) {
+    $(document).ready(function(){
+        var loading = $('#loading');
+        $.getJSON(urls.api_users, function(result) {
+            var dropdown = $("#user_id");
+            $.each(result, function(item) {
+                dropdown.append($("<option />").val(this.user_id).text(this.name));
+            });
+            dropdown.show();
+            loading.hide();
+        });
+    });
+})(jQuery);
